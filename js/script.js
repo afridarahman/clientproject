6c6100671221f16fe13022c84871a7382dc70673
afridarahman <afridarahman365@gmail.com>
2019-05-24 20:58:39 +0000

    almost done

--- js/script.js
+++ js/script.js
@@ -3,15 +3,17 @@ $(document).ready(function() {
 
 $("button").click(function(){
     var searchTerm = $('.search-term').val();
-    var giphyAPI = "https://www.googleapis.com/books/v1/volumes?q=harry%20potter";
-    $.ajax({
+    var giphyAPI = "https://www.googleapis.com/books/v1/volumes?q=" + searchTerm;
+$.ajax({
         url: giphyAPI,
         method: "GET",
         success: function(response) {
-            var bookTitle = response.items[0].volumeInfo.title
-            $('.searchResult').html(bookTitle);
+            var bookTitle = response.items[0].volumeInfo.title;
+            var bookAuthor = response.items[0].volumeInfo.authors;
+            $('.searchResult').html(bookTitle + " " + "by" + " " + bookAuthor);
         }
     });
+
 
 });
 });
